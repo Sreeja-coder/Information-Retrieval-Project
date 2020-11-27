16820d1cff9b2a1098ce8a3fe86dded309bfba1a
Sreeja pillai <54345050+Sreeja-coder@users.noreply.github.com>
2020-11-27 00:26:04 -0700

    Add files via upload

--- cleaning_data.py
+++ cleaning_data.py
@@ -17,7 +17,7 @@ from collections import defaultdict
 print("started cleaning data")
 
 
-def remove_stopwords():
+def remove_stopwords(df2,df):
     stop = stopwords.words('english')
     pattern = r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
     ks = krovetz.PyKrovetzStemmer()
@@ -66,6 +66,38 @@ def remove_stopwords():
     return new_word_final, new_word_questions
 
 
+#### for testing ######
+
+def remove_stopwords_testing(df2):
+    output = []
+    stop = stopwords.words('english')
+    pattern = r'(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))';
+    ks = krovetz.PyKrovetzStemmer()
+    new_word_final = []
+    for i in range(len(df2)):
+        asin = df2['asin'].iloc[i]
+        line = df2['description'].iloc[i]
+        line = line.strip('\[ \]')
+        match = re.findall(pattern, line)
+        for m in match:
+            url = m[0]
+            line = line.replace(url, '')
+        soup = BeautifulSoup(unescape(line), 'lxml')
+        line = soup.text
+        line = ' '.join([word.strip('; : ! * - \\ · . ®️ \®️ //') for word in line.split() if word not in (stop)])
+        tokenizer = nltk.RegexpTokenizer(r"\w+")
+        text = tokenizer.tokenize(line)
+        check_pos = lambda pos: pos[:] in ['NN', 'NNP', 'CD', 'NNS', 'JJ', 'NNPS', 'VBD', 'POS']
+        new_words = [x for (x, pos) in nltk.pos_tag(text) if check_pos(pos)]
+        #         new_words = line.split()
+        krovetz_stem_word(new_words)
+        output.append([asin,krovetz_stem_word(new_words)])
+
+    return output
+
+
+
+
 def krovetz_stem_word(word):
     ks = krovetz.PyKrovetzStemmer()
     stemmed_list = []
@@ -97,11 +129,22 @@ def query_tokenize(line):
     #         new_words = line.split()
     return (krovetz_stem_word(new_words))
 
+## training data FRAME
+df_main=pd.read_csv('pcikle_to_csv.csv',encoding='utf-8')
+df2_main = df_main.copy()
+df2_main = df2_main.loc[:, ~df2_main.columns.str.contains('^Unnamed')]
+df2_main = df2_main.drop_duplicates(subset = ["asin"])
+df2_main = df2_main.reset_index(drop=True)
 
-df=pd.read_csv('pcikle_to_csv.csv',encoding='utf-8')
-df2 = df.copy()
-df2 = df2.loc[:, ~df2.columns.str.contains('^Unnamed')]
-df2 = df2.drop_duplicates(subset = ["asin"])
-df2 = df2.reset_index(drop=True)
-cleaned_words,cleaned_questions = remove_stopwords()
+#testing data frame
+df3_main=pd.read_csv('electronics1.csv',encoding='utf-8')
+df3_main=df3_main[-200:]
+df4_main = df3_main.copy()
+df4_main = df4_main.loc[:, ~df3_main.columns.str.contains('^Unnamed')]
+df4_main = df4_main.drop_duplicates(subset = ["asin"])
+df4_main = df4_main.reset_index(drop=True)
+
+
+cleaned_words,cleaned_questions = remove_stopwords(df2_main,df_main)
+eval_output= remove_stopwords_testing(df4_main)
 print("cleaning data ended")
---
+++ evaluation.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import nltk
+import csv
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import numpy as np
+from nltk.corpus import wordnet
+import re
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet as wn
+
+
+
+## *******" gen and og"******** ##
+
+def eval_gen_og(gen,og):
+    score = 0
+    for i in gen:
+        best = 0
+        w1 = wn.synsets(i)
+        if (len(w1) > 0):
+            for j in og:
+                w2 = wn.synsets(j)
+                if len(w2) < 1:
+                    continue
+                temp = w1[0].path_similarity(w2[0])
+                if temp != None and temp > best:
+                    best = temp
+        else:
+            if i in og:
+                best = 1
+        #     print(best)
+        score = score + best
+
+    return score / len(gen)
+
+
+
+## ************"og and query "**************** ##
+def eval_og_query(og,query):
+    score = 0
+    for i in og:
+        best = 0
+        w1 = wn.synsets(i)
+        if(len(w1)>0):
+            for j in query:
+                w2 = wn.synsets(j)
+                if len(w2)<1:
+                    continue
+                temp = w1[0].path_similarity(w2[0])
+                if temp!= None and temp > best:
+                    best = temp
+        else:
+            if i in query:
+                best = 1
+    #     print(best)
+        score = score + best
+    return score/len(og)
+
+### ****** "gen and query " ***** ####
+def eval_gen_query(gen,query):
+    score = 0
+    for i in gen:
+        best = 0
+        w1 = wn.synsets(i)
+        if (len(w1) > 0):
+            for j in query:
+                w2 = wn.synsets(j)
+                if len(w2) < 1:
+                    continue
+                temp = w1[0].path_similarity(w2[0])
+                if temp != None and temp > best:
+                    best = temp
+        else:
+            if i in query:
+                best = 1
+        #     print(best)
+        score = score + best
+
+    return score / len(gen)
+
+if __name__ == "__main__":
+    tokenizer = nltk.RegexpTokenizer(r"\w+")
+    stop = stopwords.words('english')
+    df = pd.read_csv('input_for_eval.csv')
+    df3_main = pd.read_csv('electronics1.csv', encoding='utf-8')
+    ultimate_score  = []
+    df3_main = df3_main[-200:]
+    for i in df.index:
+        asin = df.asin[i]
+        query = df.query[i]
+        gen_questions = df.genQuestions[i]
+        x = df.loc[df['asin'] == str(asin)]
+        og_questions = x.question.tolist()
+
+        og = []
+        gen = []
+        for i in og_questions:
+            text = tokenizer.tokenize(i)
+            og.extend(text)
+        for i in gen_questions:
+            text = tokenizer.tokenize(i)
+            gen.extend(text)
+        og = [x.lower() for x in og]
+        gen = [x.lower() for x in gen]
+        og = list(set(og))
+        gen = list(set(gen))
+        og = [word.strip('; : ! * - \\ · . ® \® //') for word in og if word not in (stop)]
+        gen = [word.strip('; : ! * - \\ · . ® \® //') for word in gen if word not in (stop)]
+        ultimate_score.append([eval_gen_og(gen,og),eval_gen_query(gen,query),eval_og_query(og,query)])
+
+    output_eval = pd.DataFrame(ultimate_score, columns=['comparison', 'model', 'original'])
+    output_eval.to_csv('eval_output.csv')
